44c22199eb8def508a88ce17fb0e14416bdae82e
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2024-01-30 09:16:14 -0800

    idk

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ function encrypt() {
 
     let result = '';
     let inputString = document.getElementById('encrypt').value;
-  
+  console.log(inputString)
     for (let i = 0; i < inputString.length; i++) {
       let currentChar = inputString.charAt(i);
   
       // Check if the character is a lowercase letter
       if (/[a-z]/.test(currentChar)) {
-        // Convert the character to its ASCII code, shift by 1, and convert back to a character
+        // Convert the character to its ASCII code, shift by idk, and convert back to a character
         result += String.fromCharCode((currentChar.charCodeAt(0) - 97 + 4) % 26 + 97);
       }
-      // Check if the character is an uppercase letter
+      // Check if the character is something else
       else {
-        // Convert the character to its ASCII code, shift by 29, and convert back to a character
+        // Convert the character to its ASCII code, shift by how many i feel like lol, and convert back to a character
         result += String.fromCharCode((currentChar.charCodeAt(0) - 65 + 29) % 26 + 65);
       }
       
